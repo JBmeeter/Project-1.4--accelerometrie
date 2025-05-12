3b730565df66f783b7783d71d6e1f8d2812c100f
nathanmeeter <nathanmeeter@gmail.com>
2025-05-12 12:50:35 +0200

    toevoegen van alle relevante bestanden voor project 1.4

--- Oefenbestand python metingen.py
+++ Oefenbestand python metingen.py
@@ -8,45 +8,50 @@ Created on Fri Apr 25 13:40:30 2025
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from scipy.integrate import cumtrapz
 import numpy as np
-
 
 # Set working directory to script's folder
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-# Get and print the current working directory
-current_directory = os.getcwd()
-print("Current Working Directory:", current_directory)
-
 #inlezen van het csv bestand
-df = pd.read_csv('Project oefenbestand 1.csv')
+df = pd.read_csv('versnellingsprofiel_scherp.csv')
 
-t = df["Data Set 1:Time(s)"]
-a_meet = df["Data Set 1:Acceleration(m/s²)"]
-v_meet = df["Data Set 1:Velocity(m/s)"]
-d_meet = df["Data Set 1:Position(m)"]
+t = df["# tijd (s)"]
+a = df[" versnelling (m/s^2)"]
 
-#bepaling van startwaarden 
+#bepaling van constantes
 m = 5e-2
 k = 2
-b = 2*np.sqrt(m*k)
-x0 = 5e-2
+b = 2*((m*k)**.5)
+x0 = 0.2
+v0 = 0
+dt = t[1]-t[0]
 
-#afstandsfunctie
-def x_massa(m, k, b, x0, x, v,):
-    x_m = 
-    return x_m
+#positie en eerste stap
+x = np.zeros_like(t)
+x[0] = x0
+x[1] = x0 + dt*v0
+
+#snelheid en eerste stap
+v = np.zeros_like(t)
+v[0] = v0
+v[1] = v0 + a[0]*dt
+
+#for loop voor het berekenen van de positie van de massa TOV de sensor over de tijd
+for i in range(1, len(t)-1):
+    v[i+1] = v[i] + a[i]*dt
+    x[i+1] = (m*(a[i+1])-(b*v[i+1]))/k + x0
+    
 
 #plotten van de afstand werkelijk
-plt.plot(t, d_meet, label="afstand gemeten (m)")
-plt.ylabel("afstand gemeten (m)")
+plt.plot(t, x, label="afstand massa (m)")
+plt.ylabel("afstand massa (m)")
 plt.xlabel("tijd (s)")
 plt.grid()
 plt.show()
 
-plt.plot(t, a_meet, label="versnelling gemeten (m/s^2)")
+plt.plot(t, a, label="versnelling gemeten (m/s^2)")
 plt.ylabel("versnelling (m/s)²")
 plt.xlabel("tijd (s)")
 plt.grid()
